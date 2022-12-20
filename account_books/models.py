371f123ae7db0ad6373454783193de416cea8d35
bodymist <sky834459@gmail.com>
2022-12-20 17:29:30 +0900

    db: alter id column type to apply auto increment

--- account_books/models.py
+++ account_books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from members.models import Member
 
 class AccountBook(models.Model):
-    account_book_id = models.PositiveIntegerField(primary_key=True, auto_created=True)
+    account_book_id = models.AutoField(primary_key=True)
     written = models.DateField()
     member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
 
@@ -10,7 +10,7 @@ class AccountBook(models.Model):
         db_table = 'ACCOUNT_BOOK'
 
 class Detail(models.Model):
-    detail_id = models.PositiveIntegerField(primary_key=True, auto_created=True)
+    detail_id = models.AutoField(primary_key=True)
     amount = models.PositiveIntegerField(null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
     account_book_id = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
@@ -19,7 +19,7 @@ class Detail(models.Model):
         db_table = 'DETAIL'
 
 class ShortUrl(models.Model):
-    short_url_id = models.PositiveIntegerField(primary_key=True, auto_created=True)
+    short_url_id = models.AutoField(primary_key=True)
     url = models.CharField(max_length=2000)
     encoded = models.CharField(max_length=1000)
     expired = models.DateTimeField()
--- members/models.py
+++ members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-    member_id = models.PositiveIntegerField(primary_key=True, auto_created=True)
+    member_id = models.AutoField(primary_key=True)
     email = models.CharField(max_length=30)
     password = models.CharField(max_length=255)
 
