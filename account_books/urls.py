dea8d86ff31df584adbfbde840341389800ca7ba
bodymist <sky834459@gmail.com>
2022-12-21 15:33:01 +0900

    feat: add feature of read specific account book

--- account_books/urls.py
+++ account_books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account_books.views import *
 
 urlpatterns = [
-    path('detail', AccountBookAPIView.as_view()),
+    path('', AccountBookListAPIView.as_view()),
+    path('<int:pk>', AccountBookDetailAPIView.as_view()),
 ]
--- account_books/views.py
+++ account_books/views.py
@@ -1,21 +1,17 @@
-from rest_framework import status
+from rest_framework import status,exceptions
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from django.db import transaction
-from datetime import datetime
-
 from account_books.models import AccountBook
 from account_books.serializers import AccountBookSerializer
 
-class AccountBookAPIView(APIView):
+class AccountBookListAPIView(APIView):
     @transaction.atomic
-    def post(self, request):
-        # Authorization: Bearer access_token        
+    def post(self, request):     
         jwt_authenticator = JWTAuthentication()
         member_data, token = jwt_authenticator.authenticate(request)
         if member_data is None:
@@ -34,4 +30,25 @@ class AccountBookAPIView(APIView):
             )
         return Response(
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+class AccountBookDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return AccountBook.objects.get(account_book_id=pk)
+        except AccountBook.DoesNotExist:
+            raise exceptions.NotFound
+    @transaction.atomic
+    def get(self, request, pk):
+        jwt_authenticator = JWTAuthentication()
+        member_data, token = jwt_authenticator.authenticate(request)
+        account_book = self.get_object(pk)  # raise exception
+
+        if member_data is None or \
+        member_data.__getattribute__('member_id') == account_book.__getattribute__('member_id'):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = AccountBookSerializer(account_book)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_200_OK
         )
